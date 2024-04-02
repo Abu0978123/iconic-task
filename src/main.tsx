fd3930bdb33be6fdfe76decd50d5172ef06bd4f6
Abu Bakar <abubakar@whetstonez.com>
2024-04-03 02:08:33 +0500

    added new features of router dom

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.tsx'
 import Home from './components/home/Home.tsx'
-import About from './components/about/About.tsx'
-import Contact from './components/contact/Contact.tsx'
+import About from './components/About/About.tsx'
+import Contact from './components/Contact/Contact.tsx'
+import User from './components/User/User.tsx'
+import Github, { GitInfoLoader } from './components/GitHub/Github.tsx'
 
 // const routes = createBrowserRouter([
 //   {
@@ -34,6 +36,8 @@ const routes = createBrowserRouter(
     <Route path='/' element= {<Home/>}/>
     <Route path='/contact' element= {<Contact/>}/>
     <Route path='/about' element= {<About/>}/>
+    <Route path='/user/:userId' element= {<User/>}/>
+    <Route loader={GitInfoLoader} path='/github' element= {<Github/>}/>
     </Route>
   )
 )
